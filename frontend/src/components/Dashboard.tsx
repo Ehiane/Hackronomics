2f046f95d05a486fdd377ccea9908dd5be7184fd
Ogieriakhi17 <osazeogieriakhi@gmail.com>
2025-02-16 00:59:05 -0800

    Retrying to make avatar work

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -6,6 +6,7 @@ import AIRecommendations from "./AIRecommendations";
 import ExpenseBreakdown from "./ExpenseBreakdown";
 import WeeklySavings from "./WeeklySavings";
 import BalanceCard from "./BalanceCard";
+import Avatar3D from "./Avatar3D";
 
 const Dashboard = () => {
   return (
@@ -21,6 +22,7 @@ const Dashboard = () => {
         {/* Sidebar with Avatar */}
         <div className="w-[400px] bg-white shadow-md rounded-2xl p-6 flex flex-col items-center mr-6">
           <div className="w-full h-64">
+            
          {/*     */}
           </div>
           <h2 className="text-xl font-semibold mt-4">User Name</h2>
