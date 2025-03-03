6a81630f9c831c61c6a1f33d27865f59ae251e55
Ehiane <e.oigiagbe@wsu.edu>
2025-03-02 16:13:45 -0800

    fixed window width issue accross the web app, [register.css was taking over everything]

--- frontend/src/homePage/Register.tsx
+++ frontend/src/homePage/Register.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Register.css";
+// import "./Register.css";
 import NavbarHome from "./NavbarHome";
 import Footer from "./Footer";
 
